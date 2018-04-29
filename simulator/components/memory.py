d59778b65a53ca16d7180b262db88b24d335949d
Joseph <joseph@josephbanks.me>
2018-04-29 15:46:04 +0100

    Switch out 1D array for 2D array

--- simulator/components/memory.py
+++ simulator/components/memory.py
@@ -4,13 +4,13 @@ from ..types import BitInt
 class Memory:
     def __init__(self):
         # 16kB of cache on the 8008
-        self._memory = [BitInt() for _ in range(16384)]
+        self._memory = [[BitInt() for y in range(256)] for x in range(64)]
 
-    def allocate(self, index: int, item: BitInt):
-        self._memory[index] = item
+    def allocate(self, row: int, col: int, item: BitInt):
+        self._memory[row][col] = item
 
-    def deallocate(self, index: int):
-        self._memory[index] = BitInt()
+    def deallocate(self, row: int, col: int):
+        self._memory[row][col] = BitInt()
 
-    def get(self, index: int) -> BitInt:
-        return self._memory[index]
+    def get(self, row: int, col: int) -> BitInt:
+        return self._memory[row][col]
